8759602595c971f269a36474a14aa4e155882ef8
Nicholas Chin <nic.c3.14@gmail.com>
2020-09-24 12:24:58 -0600

    Better igra performance with index table
    List removal is bottleneck though

--- igra.cpp
+++ igra.cpp
@@ -1,7 +1,10 @@
 #include <cstdio>
 #include <string>
+#include <list>
+#include <map>
 
 using namespace std;
+
 int main(){
 	int n;
 	scanf("%d", &n);
@@ -9,18 +12,34 @@ int main(){
 	string mirkoWord;
 	string slavkoWord;
 	scanf("%s", buf);
-	string letters = string(buf);
+	string letterStr = string(buf);
+	map<char, list<int>>letters;
+	int lastPointer = letterStr.length()-1;
+	
+	for (int i = 0; i < n; i++){
+		letters[buf[i]].push_back(i);
+	}
+	for (char i = 'a'; i <= 'z'; i++){
+		letters[i].sort();
+	}
 
 	for (int i = 0; i < n; i++){
 		if (i%2 == 0){ // Mirko's turn
-			mirkoWord.push_back(letters.back());
-			letters.pop_back();
+			char c;
+			while(letterStr[lastPointer] == '.'){
+				lastPointer--;
+			}
+			c = letterStr[lastPointer];
+			mirkoWord.push_back(c);
+			letters[c].remove(lastPointer);
+			letterStr[lastPointer] = '.';
 		} else { // Slavko's turn
 			for (char bestChar='a'; bestChar <= 'z'; ++bestChar){
-				int pos = letters.rfind(bestChar);
-				if (pos != string::npos){
+				if (letters[bestChar].size() != 0){
+					int pos = letters[bestChar].back();
 					slavkoWord.push_back(bestChar);
-					letters.erase(pos,1);
+					letterStr[pos] = '.';
+					letters[bestChar].remove(letters[bestChar].back());
 					break;
 				}
 			}
--- test.cpp
+++ test.cpp
@@ -1,11 +1,13 @@
 #include <string>
 #include <iostream>
+#include <cstdio>
 
 using namespace std;
 
 int main(){
-	string a = "kb";
-	string b = "ka";
-	cout << a.compare(b) <<endl;
+	char buf[100001];
+	scanf("%s", buf);
+	string letters = string(buf);
+	letters.rfind('a');
 	return 0;
 }
