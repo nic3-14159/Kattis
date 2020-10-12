e39ba5a18bb6d52c4d7db2b1b9c6d3485d085b26
Nicholas Chin <nic.c3.14@gmail.com>
2020-10-11 21:59:05 -0600

    wheresmyinternet: solved, accepted by kattis
    Probably room for optimization

--- UAPC5/wheresmyinternet.cpp
+++ UAPC5/wheresmyinternet.cpp
@@ -1,48 +1,41 @@
 #include <iostream>
-#include <cstdio>
-#include <queue>
 #include <vector>
 #include <unordered_set>
+#include <map>
+#include <cstdio>
 
 using namespace std;
 
 int main(){
 	int houses, cables;
-	long long houseA, houseB;
+	int houseA, houseB;
 
 	cin >> houses >> cables;
-	unordered_set<long long> connections;
+	map<int, unordered_set<int>> connections;
+	
 	vector<bool> hasInternet (houses, false);
-	vector<bool> currLevel (houses, false);
-	int currSize = 0;
-	int nextSize = 0;
-	vector<bool> nextLevel (houses, false);
+	unordered_set<int> currLevel;
+	unordered_set<int> nextLevel;
 	hasInternet[0] = true;
 
 	while (cables > 0){
 		cin >> houseA >> houseB;
-		connections.insert((houseA-1)*200000 + (houseB-1));
-		connections.insert((houseB-1)*200000 + (houseA-1));
+		connections[houseA].insert(houseB);
+		connections[houseB].insert(houseA);
 		cables--;
 	}
-	currLevel[0] = true;
-	currSize++;
-	while(currSize != 0){
-		for (int testNode = 0; testNode < houses; testNode++){
-			if (currLevel[testNode]){
-				for (int i = 0; i < houses; i++){
-					if (!hasInternet[i] && connections.count(testNode*200000+i)){
-						hasInternet[i] = true;
-						nextLevel[i] = true;
-						nextSize++;
-					}
+	currLevel.insert(1);
+	while(!currLevel.empty()){
+		for (int i : currLevel){
+			for (int node : connections[i]){
+				if (!hasInternet[node-1] && !nextLevel.count(node)){
+					hasInternet[node-1] = true;
+					nextLevel.insert(node);
 				}
 			}
 		}
 		currLevel = nextLevel;
-		currSize = nextSize;
-		nextSize = 0;
-		nextLevel = vector<bool> (houses, false);
+		nextLevel.clear();
 	}
 	bool allConnected = true;
 	for (int i = 0; i < houses; i++){
