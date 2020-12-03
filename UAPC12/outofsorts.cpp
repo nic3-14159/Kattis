2391467016a2283369ef4579c690d524f60377b3
Nicholas Chin <nic.c3.14@gmail.com>
2020-12-03 16:22:28 -0700

    outofsorts: Improve performance
    Removed usage of set, which has too much overhead.
    Switched to sorting sequence and iterating, checking for
    unique values.

--- UAPC12/outofsorts.cpp
+++ UAPC12/outofsorts.cpp
@@ -1,24 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main(){
 	long long n, m, a, c, x0;
 	int count = 0;
 	cin >> n >> m >> a >> c >> x0;
 	vector<long long> seq(n, 0);
-	set<long long> unique;
 	for (int i = 0; i < n; i++){
 		if (i == 0){
 			seq[0] = (a*x0 + c) % m;
 		} else {
 			seq[i] = (a*seq[i-1] + c) % m;
 		}
-		unique.insert(seq[i]);
 	}
-	for (auto i : unique){
+	vector<long long> unique(seq);
+	sort(unique.begin(), unique.end());
+	long long last = -1;
+	for (auto i : seq){
 		int low = 0;
 		int high = seq.size()-1;
 		int mid;
+		if (i == last){
+			continue;
+		}
+		last = i;
 		while (low <= high) {
 			mid = (low+high)/2;
 			if (seq[mid] == i){
@@ -31,7 +35,6 @@ int main(){
 				low = mid+1;
 			}
 		}
-
 	}
 	cout << count << endl;
 	return 0;
